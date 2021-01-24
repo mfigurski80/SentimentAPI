9c191ae2292764aeace34613c7700a9944b53d9c
Mikolaj Figurski <meek.f80@gmail.com>
2021-01-24 01:14:26 -0500

    Add scanning utility funcs

--- client/types.go
+++ client/types.go
@@ -1,14 +1,16 @@
 package client
 
+import "database/sql"
+
 type Tweet struct {
-	ID          int
-	Time        int
-	TimePosted  int
-	Sentiment   string
-	Confidence  int
-	Text        string
-	Username    string
-	TwitterLink string
+	ID         int
+	Time       int
+	CreatedAt  int
+	Sentiment  string
+	Confidence int
+	Text       string
+	Username   string
+	Link       string
 }
 
 type Point struct {
@@ -17,4 +19,35 @@ type Point struct {
 	Negative int
 	Retweets int
 	Total    int
+}
+
+func scanTweet(scanner *sql.Rows) *Tweet {
+	var t Tweet
+	if err := (*scanner).Scan(
+		&t.ID,
+		&t.Time,
+		&t.CreatedAt,
+		&t.Sentiment,
+		&t.Confidence,
+		&t.Text,
+		&t.Username,
+		&t.Link,
+	); err != nil {
+		panic(err.Error())
+	}
+	return &t
+}
+
+func scanPoint(scanner *sql.Rows) *Point {
+	var p Point
+	if err := (*scanner).Scan(
+		&p.Time,
+		&p.Positive,
+		&p.Negative,
+		&p.Retweets,
+		&p.Total,
+	); err != nil {
+		panic(err.Error())
+	}
+	return &p
 }
