d7c7fadf7479e6a063b1354636ba42bf42254848
Mikolaj Figurski <meek.f80@gmail.com>
2021-01-24 22:27:37 -0500

    Finish cache implementation

--- cache/point.go
+++ cache/point.go
@@ -1,19 +1,56 @@
 package cache
 
-import "github.com/mfigurski80/SentimentAPI/types"
+import (
+	"fmt"
+
+	"github.com/mfigurski80/SentimentAPI/client"
+	"github.com/mfigurski80/SentimentAPI/types"
+)
 
 type PointCacheType []types.Point
 
 var PointCache = make(PointCacheType, 0)
 
+func ReadPoints(from int64, to int64) []types.Point {
+	queryFrom, queryTo, cacheUpdateFunc := getUncachedRange(from, to)
+	countUpdated := updateCacheForRange(queryFrom, queryTo, cacheUpdateFunc)
+	if countUpdated != 0 {
+		fmt.Printf("cache updated with #%v\n", countUpdated)
+	}
+
+	lo, _ := binSearchPointCache(from)
+	hi, _ := binSearchPointCache(to)
+
+	return PointCache[lo:hi:hi]
+}
+
+func updateCacheForRange(from int64, to int64, cacheUpdateFunc UpdateFuncType) int {
+	if cacheUpdateFunc == nil {
+		return 0
+	}
+	// perform a cache update
+	query := fmt.Sprintf(
+		"SELECT * FROM TimeSeries WHERE time > \"%s\" AND time < \"%s\" ORDER BY time",
+		client.ParseUnixTime(from),
+		client.ParseUnixTime(to),
+	)
+	rows, err := client.Execute(query)
+	if err != nil {
+		panic(err.Error())
+	}
+	points := client.ReadOutPoints(rows)
+	cacheUpdateFunc(points)
+	return len(*points)
+}
+
 // GetUncachedRange subtracts given range from cached range
 // in order to provide the full range you need to pull
-func GetUncachedRange(from int64, to int64) (int64, int64, UpdateFuncType) {
+func getUncachedRange(from int64, to int64) (int64, int64, UpdateFuncType) {
 	// Cache: ------[----------]-----
 	// Range: ---------[--]---------- : Dont query
 	// Range: ----[---------]-------- : Query reduced range on left
 	// Range: [--]------------------- : Query increased range on left
-	// Range: [---------------------] : Query everything
+	// Range: [---------------------] : Query everything and replace cache
 	if len(PointCache) == 0 {
 		return from, to, updateAll
 	}
